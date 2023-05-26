dc0d66c85c7866a5c1699b4ab8022e828320c3bc
youssefmo7amed <youssef.mo7amed@gmail.com>
2023-05-26 21:08:26 +0200

    22-05-2023

--- solutions-cpp/easy/1. Two Sum.cpp
+++ solutions-cpp/easy/1. Two Sum.cpp
@@ -18,3 +18,42 @@ public:
     return sol;
     }
 };
+
+// or
+
+// naive solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        bool found = false;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans = { i, j };
+                    found = true;
+                }
+            }
+            if (found) break;
+        }
+        return ans;
+    }
+};
+
+// or
+
+
+// naive solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return  { i, j };
+                }
+            }
+        }
+        return {};
+    }
+};
