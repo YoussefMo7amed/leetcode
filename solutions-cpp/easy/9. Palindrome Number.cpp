cf494961f4197250b7d1f5d14e1cc90f98dea71a
youssefmo7amed <youssef.mo7amed@gmail.com>
2023-05-26 20:59:54 +0200

    17-05-2023

--- solutions-cpp/easy/9. Palindrome Number.cpp
+++ solutions-cpp/easy/9. Palindrome Number.cpp
@@ -22,3 +22,36 @@ public:
         return true;
     }
 };
+
+// second solution
+// compare digit by digit from left and right
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        if( x == 0)
+            return true;
+        long long d = ceil(log10(x));
+        printf("%d\n", d);
+        long long left, right;
+        left = right = x;
+
+        for (int i = 0; i < d; i++) {
+            long long start = left / pow(10, d - i - 1);
+
+            left %= (long long )pow(10, d - i - 1);
+
+            long long end;
+
+            long long md = (long long) round(pow(10, i+1));
+            right = x % md;
+            long long  dv = (long long)round(pow(10, i));
+            end = right/dv;
+
+            if (start != end)
+                return false;
+        }
+        return true;
+    }
+};
